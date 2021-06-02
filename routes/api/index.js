0a9d019b0cc4bc0741899f9e1d3a671226320622
geotecdev <mikeruanedev@gmail.com>
2021-06-02 16:50:14 -0400

    set up custom api routes for matchTransactions and oldMatches

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const swipeRoutes = require("./swipe-routes");
 const userRoutes = require("./user-routes");
+const matchTransactionRoutes = require("./matchTransaction-routes");
+const oldMatchRoutes = require("./oldMatch-routes");
 
 router.use("/users", userRoutes);
 router.use("/swipes", swipeRoutes);
+router.use("/matchtransactions", matchTransactionRoutes);
+router.use("/oldmatches", oldMatchRoutes);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,79 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Swipe } = require("../../models");
+
+router.get("/", async (req, res) => {
+    // find all users
+    try {
+      const allUsers = await User.findAll({
+        include: [{model: Swipe}],
+      });
+      res.status(200).json(allUsers);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get("/:id", async (req, res) => {
+    // find one user by `id` value
+    try {
+      const user = await User.findByPk(req.params.id, {
+          include: [{model: Swipe}]
+      });
+      if (!user) {
+          res.status(404).json({message: "no users with the specified id exist in the database"});
+          return;
+      }
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.post("/", async (req, res) => {
+    // create a new user
+    try {
+        //console.log(req.body);
+        const newUser = await User.create(req.body);
+        res.status(200).json(newUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+  });
+  
+  router.put("/:id", async (req, res) => {
+    // update a user by `id` value
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!userData) {
+            res.status(400).json({ message: "Update failed: no user with the specified id was found" });
+            return;
+        }
+        res.status(200).json(userData);
+      } catch (err) {
+          res.status(500).json(err);
+      }
+  });
+  
+  router.delete("/:id", async (req, res) => {
+    // delete a user by `id` value
+    try {
+        const delUser = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+          });
+          if (!delUser) {
+              res.status(400).json({ message: "Delete failed: no user with the specified id was found" });
+              return;
+          }
+          res.status(200).json(delUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
